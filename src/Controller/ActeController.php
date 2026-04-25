59c9446d38cbe9fb310e9dad56d298b858488828
unknown <a.andjib@yahoo.fr>
2026-04-25 09:02:56 +0200

    Resolu: Chargement ajax et  Affichage de la liste des documents en fonction de la prefecture choisie

--- src/Controller/ActeController.php
+++ src/Controller/ActeController.php
@@ -6,9 +6,13 @@ use App\Entity\Acte;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\ActeRepository;
+use Doctrine\DBAL\Connection;
+
 
 
 class ActeController extends AbstractController
