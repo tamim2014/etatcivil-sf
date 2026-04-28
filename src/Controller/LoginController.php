212e405b43e748ecd6917ee596ffd4abc891483a
unknown <a.andjib@yahoo.fr>
2026-04-28 02:37:05 +0200

    loging usermanagement

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -7,26 +7,34 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 use Symfony\Component\HttpFoundation\Request;//pour le formulaire
+use App\Repository\ListeofficiersRepository;
 
 class LoginController extends AbstractController
 {
     #[Route('/login', name: 'login', methods: ['GET', 'POST'])]
-	public function index(Request $request): Response
+	public function index(Request $request, ListeofficiersRepository $officierRepository): Response
 	{
         $messageTextarea = "Veuillez saisir vos identifiants";
+ 
+        
         
 		if ($request->isMethod('POST')) {
 
 			$login = $request->request->get('pseudo_');
-			$mdp   = $request->request->get('motdepasse_');
-			
+			//$mdp   = $request->request->get('motdepasse_');
 
-			if ($login === "usermanagement" && $mdp === "8888") {
+			$officier = $officierRepository->findOneBy([
+				'pseudo' => $login
+			]);
+			$roles = $officier->getRoles();
+
+			//if ($login === "usermanagement" && $mdp === "8888") {
+			if ($officier && $roles === 'usermanagement'){
 				return $this->redirectToRoute('usermanagement');
 			}
 			$this->addFlash('error', 'Identifiants incorrects');
             return $this->redirectToRoute('login');	
-           		   
+           	   
 		}
 
 				
