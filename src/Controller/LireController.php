5f55a078cb58502cd0e35245ce9561eb7f5e63e4
unknown <a.andjib@yahoo.fr>
2026-05-03 20:38:28 +0200

    Commit de backup: Debug lireController/findByPrefecture()

--- src/Controller/LireController.php
+++ src/Controller/LireController.php
@@ -34,8 +34,17 @@ final class LireController extends AbstractController
             'message' => $message,
         ]);
     }
-
-
+    /*
+     * Ce controleur a 3 missions:
+     * 
+     * 1. Récuperer le filtre "prefecture"
+     *    (transmis en GET par capture_items.js)
+     * 2. Stocker ce filtre dans une session
+     *    (pour que lectureBD.html.twig affiche la prefecture) 
+     * 3. Faire un select(construire un findBy) par ce filtre 
+     *    (pour que son template puisse remplir la table)
+     * 
+     */
     #[Route('/tablenaissance/{pr}', name: 'tablenaissance')]
     public function tableNaissance(
         string $pr,
@@ -43,12 +52,12 @@ final class LireController extends AbstractController
         RequestStack $requestStack
         
     ): Response {
-
+        //1. Recupere le filtre(tramis par capture_items.js)
         $pref = ltrim($pr);
-
+        //2. Stocke le filtre dans une session
         $session = $requestStack->getSession();
         $session->set('pref', $pref);
-
+        //3. Construit un findBy(pour faire un select par ce filtre)
         $lignes = $repo->findByPrefecture($pref);
   
 
